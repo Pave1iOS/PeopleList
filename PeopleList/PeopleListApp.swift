f793961d3ea9d8d026fb59e7f0cfc70e205c42c2
Pave1iOS <PGribachev94@yandex.ru>
2024-03-14 17:19:12 +0300

    Initial Commit

---
+++ PeopleList/PeopleListApp.swift
@@ -0,0 +1,17 @@
+//
+//  PeopleListApp.swift
+//  PeopleList
+//
+//  Created by Pavel Gribachev on 14.03.2024.
+//
+
+import SwiftUI
+
+@main
+struct PeopleListApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
