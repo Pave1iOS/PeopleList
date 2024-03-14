fff883a28342ce2647a73098f0f323acd47067ee
Pave1iOS <PGribachev94@yandex.ru>
2024-03-14 17:22:54 +0300

    start project

---
+++ PeopleList/User.swift
@@ -0,0 +1,15 @@
+//
+//  User.swift
+//  PeopleList
+//
+//  Created by Pavel Gribachev on 14.03.2024.
+//
+
+import Foundation
+
+struct User {
+    let name: String
+    let lastName: String
+    let email: String
+    let phoneNumber: String
+}
